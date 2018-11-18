86c70fc3f7288770886e81e835fea004202fe4f9
Karl Blair <kblr1@hotmail.com>
2018-11-18 22:21:48 +0000

    Second commit with Employee class implemented

--- CA2/src/Employee.java
+++ CA2/src/Employee.java
@@ -1,3 +1,6 @@
+
+import java.util.Calendar;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +13,47 @@
  */
 
 public class Employee {
+    private int id;
+    private String name;
+    private Calendar dob;
+    private Calendar rdate;
     
+    public Employee(String name, int id, Calendar dob, Calendar startDate){
+        this.name = name;
+        this.id = id;
+        this.dob = startDate;
+        this.rdate = rdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Calendar getDob() {
+        return dob;
+    }
+
+    public Calendar getRdate() {
+        return rdate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDob(Calendar dob) {
+        this.dob = dob;
+    }
+
+    public void setRdate(Calendar rdate) {
+        this.rdate = rdate;
+    }
 }
