4a01ea4c6fe8be68f3cf675822b6d5e49fe040de
X00149335 <X00149335@computing.stu.it-tallaght.ie>
2018-11-20 10:39:17 +0000

    Moving some of the files around and filling out the employee operations class

---
+++ CA2/src/DB/EmployeeOperations.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB;
+
+/**
+ *
+ * @author x00149335
+ */
+
+import java.sql.*;
+import oracle.jdbc.pool.OracleDataSource;
+public class EmployeeOperations {
+    
+    private Connection conn;
+    private PreparedStatement pstmt;
+    
+    public Connection openDB() {
+        try {
+            OracleDataSource ods = new OracleDataSource();
+
+            // Tallaght
+//             ods.setURL("jdbc:oracle:thin:@//10.10.2.7:1521/global1");
+//             ods.setUser("pmagee");
+//             ods.setPassword("tupulo");
+            // Home Oracle XE
+            ods.setURL("jdbc:oracle:thin:@//10.10.2.7:1521/global1");
+            ods.setUser("x00149335");
+            ods.setPassword("db01Nov99");
+
+            conn = ods.getConnection();
+            System.out.println("connected.");
+        } catch (SQLException e) {
+            System.out.print("Unable to load driver " + e.getMessage());
+        }
+        return conn;
+    }
+    
+    public void dropEmployeeSequence() {
+        try {
+            String s2 = "drop sequence eid_seq";
+            pstmt = conn.prepareStatement(s2);
+            pstmt.executeUpdate();
+            System.out.println("Employee Sequence dropped");
+        } catch (SQLException ex) {
+            
+        }
+    }
+    
+    public void createEmployeeSequence() {
+        // Creating a sequence    
+        try {
+            String createseq1 = "create sequence eid_seq increment by 1 start with 1";
+            pstmt = conn.prepareStatement(createseq1);
+            pstmt.executeUpdate();
+            System.out.println("Employee Sequence created");
+        } catch (SQLException ex) {
+            System.out.print("Problem with Employee Sequence " + ex.getMessage());
+        }
+
+    }
+    
+    public void dropEmployeeTable() {
+        System.out.println("Checking for existence of Employee table");
+        try {
+            String s1 = "DROP TABLE Employee2";
+            pstmt = conn.prepareStatement(s1);
+            pstmt.executeUpdate();
+            System.out.println("Employee table dropped");
+        } catch (SQLException ex) {
+            
+        }
+    }
+    
+    public void createEmployeetable() {
+        // Create a Table           
+        try {
+            String sql = "CREATE TABLE Employees (empid NUMBER PRIMARY KEY "
+                    + "NOT NULL,"
+                    + "type VARCHAR2(5),"
+                    + "name VARCHAR2(40),"
+                    + "startDate DATE,"
+                    + "salary DECIMAL(8,2),"
+                    + "rate DECIMAL(5,2)," 
+                    + "hours DECIMAL(5,2))";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.out.println("SQL Exception creating "
+                    + "Employee table" + ex.getMessage());
+        }
+    }
+    
+    public void fillStudentTable() {
+        try {
+            // Insert data into table
+            String sql = "INSERT INTO Employee(id,name,dob,startDate) "
+                    + "values(sid_seq.nextVal,?,?,?)";
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, "Karl Blair");
+            pstmt.setDate(2, Date.valueOf("1999-01-23"));
+            pstmt.setDate(3, Date.valueOf("2018-09-13"));
+            pstmt.executeUpdate();
+
+            pstmt.setString(1, "Adam Brennan");
+            pstmt.setDate(2, Date.valueOf("1997-02-23"));
+            pstmt.setDate(3, Date.valueOf("2018-09-13"));
+            pstmt.executeUpdate();
+
+            pstmt.setString(1, "David Bird");
+            pstmt.setDate(2, Date.valueOf("1990-03-04"));
+            pstmt.setDate(3, Date.valueOf("2018-09-25"));
+            pstmt.executeUpdate();
+
+            pstmt.setString(1, "Paris Hilton");
+            pstmt.setDate(2, Date.valueOf("1996-02-19"));
+            pstmt.setDate(3, Date.valueOf("2018-09-28"));
+            pstmt.executeUpdate();
+
+            pstmt.setString(1, "John Joe");
+            pstmt.setDate(2, Date.valueOf("1980-08-16"));
+            pstmt.setDate(3, Date.valueOf("2018-09-25"));
+            pstmt.executeUpdate();
+
+            System.out.println("Employee table populated");
+        } catch (SQLException ex) {
+            System.out.println("SQL Exception inserting values into "
+                    + "Employee table" + ex.getMessage());
+        }
+    }
+    
+}
