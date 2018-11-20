c17b422cd598ca3b468fde0762e72b8b06fbe79d
X00149335 <X00149335@computing.stu.it-tallaght.ie>
2018-11-20 10:56:07 +0000

    Adding more functionality to the PersistanceOperations and making the FullTimeEmployee class

---
+++ CA2/src/DB/PersistanceOperations.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB;
+
+/**
+ *
+ * @author x00149335
+ */
+import javax.persistence.*;
+import java.sql.*;
+import java.util.Calendar;
+import java.util.List;
+import model.Employee;
+import model.FullTimeEmployee;
+import model.PartTimeEmployee;
+
+public class PersistanceOperations {
+
+    EntityManagerFactory emf;
+    EntityManager em;
+
+    public PersistanceOperations() {
+        emf = Persistence.createEntityManagerFactory("CA2PU");
+        em = emf.createEntityManager();
+    }
+
+    public void showAllEmployess() {
+        em.getTransaction().begin();
+
+        TypedQuery<Employee> query
+                = em.createQuery("SELECT e FROM Employees e order by e.ID",
+                        Employee.class);
+        List<Employee> results = query.getResultList();
+        for (Employee e : results) {
+            System.out.println(e);
+        }
+        em.getTransaction().commit();
+    }
+
+    public void showAllPT() {
+        em.getTransaction().begin();
+        TypedQuery<PartTimeEmployee> query
+                = em.createQuery("SELECT pt FROM PartTimeEmployee pt "
+                        + "order by pt.ID", PartTimeEmployee.class);
+        List<PartTimeEmployee> results = query.getResultList();
+        for (PartTimeEmployee pt : results) {
+            System.out.println(pt);
+        }
+        em.getTransaction().commit();
+    }
+    
+    public void showALLFT(){
+        em.getTransaction().begin();
+        TypedQuery<FullTimeEmployee> query
+                = em.createQuery("SELECT ft FROM FullTimeEmployee ft "
+                        + "order by ft.ID", FullTimeEmployee.class);
+        List<FullTimeEmployee> results = query.getResultList();
+        for (FullTimeEmployee ft : results) {
+            System.out.println(ft);
+        }
+        em.getTransaction().commit();
+    }
+}
