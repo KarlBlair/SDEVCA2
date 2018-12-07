930b8e06097d3830f1a2b71a656d7eec14a46d58
X00149095 <X00149095@computing.stu.it-tallaght.ie>
2018-12-07 16:55:39 +0000

    output layouts Done

--- CA2/src/Test/GameStoreDBTest.java
+++ CA2/src/Test/GameStoreDBTest.java
@@ -59,90 +59,66 @@ public class GameStoreDBTest {
         System.out.println("zz4");
 
         PersistanceOperations po = new PersistanceOperations();
-
+        outputCommands();
+        
         while (run) {
-            System.out.println("Please press 1 to add a new Employee");
-            System.out.println("Please press 2 to remove an Employee");
-            System.out.println("Please press 3 to add a new Manager");
-            System.out.println("Please press 4 to remove a Manager");
-            System.out.println("Please press 5 to view all Employees");
-            System.out.println("Please press 6 to view all Games on Record");
-            System.out.println("Please press 7 to add a new game");
-            System.out.println("Please press 8 to remove a game");
-            System.out.println("Please press 9 to view all available Developers");
-            System.out.println("Please press 10 to add a new developer record");
-            System.out.println("Please press 11 to remove a developer record");
-            System.out.println("Please press 12 to update a Developer ");
-            System.out.println("13 for orders");
-            System.out.println("Please press 16 to exit");
-
+            System.out.print("\nOption Number: ");
             int choice = in.nextInt();
             in.nextLine();
-
+            
             switch (choice) {
                 case 1:
-                    System.out.println("Enter the name of the Employee you want to add ");
+                    System.out.print("Enter the name of the Employee you want to add: ");
                     String name = in.nextLine();
                     po.addEmployee(name);
                     break;
-                case 2:
-                    System.out.println("Enter the ID of the Employee you want to remove ");
+                case 4:
+                    System.out.print("Enter the ID of the Employee you want to remove: ");
                     int empid = in.nextInt();
                     po.removeEmployee(empid);
                     break;
-                case 3:
-                    System.out.println("Enter the name of the Manger you want to add ");
-                    String mName = in.nextLine();
-//                    po.addManager(mName);
-                    break;
-                case 4:
-                    System.out.println("Enter the name of the Manager you want to remove ");
-                    String mID = in.nextLine();
-//                    po.removeManager(mID);
-                    break;
-                case 5:
+                case 10:
                     po.showAllEmployees();
                     break;
-                case 6:
+                case 11:
                     po.showAllGames();
                     break;
-                case 7:
-                    System.out.println("Please enter the name of the game you would like to add ");
+                case 8:
+                    System.out.print("Please enter the name of the game you would like to add: ");
                     String gameName = in.nextLine();
 
-                    System.out.println("Please enter the name of the Game Genre for the game that you added ");
+                    System.out.print("Please enter the name of the Game Genre for the game that you added: ");
                     String gameGenre = in.nextLine();
                     po.addGame(gameName, gameGenre);
                     break;
-                case 8:
-                    System.out.println("Please enter the ID of the game you would like to remove ");
+                case 5:
+                    System.out.print("Please enter the ID of the game you would like to remove: ");
                     int gameID = in.nextInt();
                     po.removeGame(gameID);
                     break;
-                case 9:
+                case 12:
                     po.showAllDevelopers();
                     break;
-                case 10:
-                    /* do we want to have the employee names? or just companies? ask tomorrow */
-                    System.out.println("Please enter the name of the development company ");
+                case 3:
+                    System.out.print("Please enter the name of the development company: ");
                     String companyName = in.nextLine();
-                    System.out.println("How many years has this company been in business ");
+                    System.out.print("How many years has this company been in business: ");
                     int yearsActive = in.nextInt();
 
                     po.addDeveloper(companyName, yearsActive);
                     break;
-                case 11:
-                    System.out.println("Please enter the ID of the developer you wish to remove ");
+                case 6:
+                    System.out.print("Please enter the ID of the developer you wish to remove: ");
                     int devID = in.nextInt();
                     po.removeDeveloper(devID);
                     break;
-                case 12:
-                    System.out.println("Please enter the ID of the Developer you want to update");
+                case 9:
+                    System.out.print("Please enter the ID of the Developer you want to update: ");
                     int ID = in.nextInt();
                     in.nextLine();
-                    System.out.println("Please enter the new name");
+                    System.out.print("Please enter the new name: ");
                     String newName = in.nextLine();
-                    System.out.println("Please enter the new years active");
+                    System.out.print("Please enter the new years active: ");
                     int newYears = in.nextInt();
                     
                     po.updateDeveloper(ID, newName, newYears);
@@ -150,13 +126,45 @@ public class GameStoreDBTest {
                 case 13:
                     po.showAllOrders();
                     break;
-                case 16:
+                case 14:
                     po.close();
                     run = false;
                     break;
+                case 15:
+                    outputCommands();
+                    break;
+                default:
+                    System.out.printf("%d is not a valid option. Please try again!\n", choice);
             }
-
         }
+    }
+    
+    public static void outputCommands() {
+        System.out.println("\n=======================================");
+        System.out.println("Add Options:");
+        System.out.println("1] New Employee");
+        System.out.println("2] New Game");
+        System.out.println("3] New Developer");
 
+        System.out.println("\nRemove Options:");
+        System.out.println("4] Remove an Employee");
+        System.out.println("5] Remove a Game");
+        System.out.println("6] Remove a Developer");
+
+        System.out.println("\nUpdate Options:");
+        System.out.println("7] Update an Employee");
+        System.out.println("8] Update a Game");
+        System.out.println("9] Update a Developer");
+
+        System.out.println("\nView Options:");
+        System.out.println("10] View all Employees");
+        System.out.println("11] View all Games on Record");
+        System.out.println("12] View all available Developers");
+        System.out.println("13] View all Orders");
+
+        System.out.println("\nMisc:");
+        System.out.println("14] Exit the program");
+        System.out.println("15] Output this command list again");
+        System.out.println("=======================================\n");
     }
 }
